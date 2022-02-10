5708bcd2eaf7c0dc87fde19915efd973ae4c33be
sinka27 <sinka27392@gmail.com>
2022-02-10 14:07:48 -0800

    Added alert once user is logged in

--- public/js/login.js
+++ public/js/login.js
@@ -13,6 +13,7 @@ const loginFormHandler = async (event) => {
 
     if (response.ok) {
       document.location.replace('/');
+      alert('Login Successful');
     } else {
       alert('Failed to log in');
     }
