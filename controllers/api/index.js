b38c1f3a9cb3b802ce9fe02394ee4d315e652821
sinka27 <sinka27392@gmail.com>
2022-02-14 01:26:20 -0800

    Adding routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,12 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const artRoutes = require('./art-routes').router;
+const addToRegistry = require('./postAddToRegistry');
+const addRemoveLikes = require('./postAddRemoveLikes');
 
 router.use('/users', userRoutes);
+router.use('/', addToRegistry);
+router.use('/', addRemoveLikes);
+router.use('/art', artRoutes);
 
 module.exports = router;
